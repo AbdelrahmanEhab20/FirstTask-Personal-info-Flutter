53674f1ba47c3c42b1deb6180ca0fcb76e2e2373
AbdelrahmanEhab <57192912+AbdelrahmanEhab20@users.noreply.github.com>
2022-06-13 10:01:44 +0200

    add custom icons/fonts/images + structure the app

---
+++ lib/icons.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/widgets.dart';
+
+class CustomIcons {
+  CustomIcons._();
+
+  static const _kFontFam = 'CustomIcons';
+  static const String? _kFontPkg = null;
+
+  static const IconData person_icon =
+      IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData date_icon =
+      IconData(0xe801, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData country_icon =
+      IconData(0xe802, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData state_icon =
+      IconData(0xe803, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData cloud_icon =
+      IconData(0xe804, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData logo_stract =
+      IconData(0xe805, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData male_icon =
+      IconData(0xe806, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData female_icon =
+      IconData(0xe807, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData gender_icon =
+      IconData(0xe808, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:hexcolor/hexcolor.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:task_one/icons.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,51 +17,185 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Task-One',
-      theme: ThemeData(
-        primaryColor: HexColor("#161B45"),
-      ),
+      //
       home: Scaffold(
-        backgroundColor: Color(0xFFF7F7F7),
+        backgroundColor: Color.fromRGBO(247, 247, 247, 1),
         appBar: AppBar(
           backgroundColor: Color(0xFFF7F7F7),
           elevation: 0,
-          title: Image.asset(
-            'assets/images/logo_stract.png',
-            fit: BoxFit.cover,
-          ),
+          title: Image.asset('assets/images/logo_stract.png'
+              // 'assets/icons/logo_stract.svg',
+              // fit: BoxFit.cover,
+              ),
         ),
-        body: Container(
-          child: Column(
-            children: [
-              Container(
-                child: Center(
-                    heightFactor: 3.5,
-                    child: Text(
-                      'Personal Information',
-                      style: TextStyle(
-                          color: Color(0xFF161B45),
-                          fontFamily: 'Avenir LT Std',
-                          fontSize: 30,
-                          fontWeight: FontWeight.w900),
-                    )),
-              ),
-              BuildTextFieldContainer(
-                customchild: TextField(
-                  decoration: InputDecoration(
-                      border: InputBorder.none,
-                      icon: Icon(Icons.person, color: Color(0xFF757575)),
-                      hintText: 'First Name*'),
+        body: SingleChildScrollView(
+          child: Container(
+            child: Column(
+              children: [
+                Container(
+                  margin: EdgeInsets.only(top: 60, bottom: 20),
+                  child: Center(
+                      child: Text(
+                    'Personal Information',
+                    style: TextStyle(
+                        color: Color(0xFF161B45),
+                        fontFamily: 'Avenir LT Std',
+                        fontSize: 30,
+                        fontWeight: FontWeight.w900),
+                  )),
                 ),
-              ),
-              BuildTextFieldContainer(
-                customchild: TextField(
-                  decoration: InputDecoration(
-                      border: InputBorder.none,
-                      icon: Icon(Icons.person, color: Color(0xFF757575)),
-                      hintText: 'Last Name*'),
+                SizedBox(height: 10),
+                BuildTextFieldContainer(
+                  customChild: TextField(
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        icon: Icon(CustomIcons.person_icon,
+                            size: 18, color: Color(0xFF757575)),
+                        hintText: 'First Name*'),
+                  ),
                 ),
-              )
-            ],
+                SizedBox(height: 10),
+                BuildTextFieldContainer(
+                  customChild: TextField(
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        icon: Icon(CustomIcons.person_icon,
+                            size: 18, color: Color(0xFF757575)),
+                        hintText: 'Last Name*'),
+                  ),
+                ),
+                SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    // Spacer(),
+                    Container(
+                      margin: EdgeInsets.symmetric(vertical: 5),
+                      padding: EdgeInsets.symmetric(horizontal: 10),
+                      width: 110,
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Color(0xFF9E9E9E)),
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      child: TextField(
+                        keyboardType: TextInputType.datetime,
+                        textAlign: TextAlign.center,
+                        decoration: InputDecoration(
+                            border: InputBorder.none,
+                            icon: Icon(CustomIcons.date_icon,
+                                size: 18, color: Color(0xFF757575)),
+                            hintText: 'Day*'),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Container(
+                      margin: EdgeInsets.symmetric(vertical: 5),
+                      padding: EdgeInsets.symmetric(horizontal: 10),
+                      width: 110,
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Color(0xFF9E9E9E)),
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      child: TextField(
+                        keyboardType: TextInputType.datetime,
+                        textAlign: TextAlign.center,
+                        decoration: InputDecoration(
+                            border: InputBorder.none,
+                            // icon:
+                            //     Icon(Icons.date_range, color: Color(0xFF757575)),
+                            hintText: 'Month*'),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Container(
+                      margin: EdgeInsets.symmetric(vertical: 5),
+                      padding: EdgeInsets.symmetric(horizontal: 10),
+                      width: 110,
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Color(0xFF9E9E9E)),
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      child: TextField(
+                        keyboardType: TextInputType.datetime,
+                        textAlign: TextAlign.center,
+                        decoration: InputDecoration(
+                            border: InputBorder.none,
+                            // icon:
+                            //     Icon(Icons.date_range, color: Color(0xFF757575)),
+                            hintText: 'Year*'),
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    // Spacer(),
+                    Container(
+                      margin: EdgeInsets.symmetric(vertical: 5),
+                      padding: EdgeInsets.symmetric(horizontal: 10),
+                      width: 110,
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Color(0xFF9E9E9E)),
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      child: TextField(
+                        textAlign: TextAlign.center,
+                        enabled: false,
+                        decoration: InputDecoration(
+                            border: InputBorder.none,
+                            icon: Icon(CustomIcons.gender_icon,
+                                size: 16, color: Color(0xFF757575)),
+                            hintText: 'Gender'),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Container(
+                      margin: EdgeInsets.symmetric(vertical: 5),
+                      padding: EdgeInsets.symmetric(horizontal: 10),
+                      width: 110,
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Color(0xFF9E9E9E)),
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      child: TextField(
+                        decoration: InputDecoration(
+                            border: InputBorder.none,
+                            icon: Icon(CustomIcons.male_icon,
+                                size: 16, color: Color(0xFF757575)),
+                            hintText: 'Male'),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Container(
+                      margin: EdgeInsets.symmetric(vertical: 5),
+                      padding: EdgeInsets.symmetric(horizontal: 10),
+                      width: 110,
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Color(0xFF9E9E9E)),
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      child: TextField(
+                        decoration: InputDecoration(
+                            border: InputBorder.none,
+                            icon: Icon(CustomIcons.female_icon,
+                                size: 16, color: Color(0xFF757575)),
+                            hintText: 'Female'),
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
@@ -69,22 +204,22 @@ class MyApp extends StatelessWidget {
 }
 
 class BuildTextFieldContainer extends StatelessWidget {
-  final Widget? customchild;
+  final Widget? customChild;
 
-  const BuildTextFieldContainer({Key? key, this.customchild}) : super(key: key);
+  const BuildTextFieldContainer({Key? key, this.customChild}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Container(
-      margin: EdgeInsets.symmetric(vertical: 10),
-      padding: EdgeInsets.symmetric(horizontal: 20),
+      margin: EdgeInsets.symmetric(vertical: 5),
+      padding: EdgeInsets.symmetric(horizontal: 10),
       width: size.width * .9,
       decoration: BoxDecoration(
         border: Border.all(color: Color(0xFF9E9E9E)),
         borderRadius: BorderRadius.circular(30),
       ),
-      child: customchild,
+      child: customChild,
     );
   }
 }
